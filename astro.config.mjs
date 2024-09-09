5377a7fad1dff6ea4026b2f4797f2d6e70a4d143
Roman Chursanov <authfailed@gmail.com>
2024-09-09 12:34:21 +0500

    build with astro

---
+++ astro.config.mjs
@@ -0,0 +1,47 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import starlight from "@astrojs/starlight";
+import starlightImageZoom from "starlight-image-zoom";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://authfailed.github.io",
+  base: "/domru-helper",
+  integrations: [
+    starlight({
+      title: "Вика по Хелперу",
+      logo: {
+        src: "./src/content/assets/logo.png",
+        replacesTitle: true,
+      },
+      locales: {
+        root: {
+          label: "Russian",
+          lang: "ru",
+        },
+      },
+      sidebar: [
+        { label: "Домашняя страница", link: "/" },
+        { label: "👋 База", link: "/wiki" },
+        { label: "🛠️ Установка расширения", link: "/wiki/setup" },
+        {
+          label: "Функции",
+          items: [
+            { slug: "wiki/functions/popup-window" },
+            { slug: "wiki/functions/settings" },
+          ],
+        },
+      ],
+      plugins: [
+        starlightImageZoom({
+          showCaptions: true,
+        }),
+      ],
+      social: {
+        telegram: "https://t.me/nedomru",
+        github: "https://github.com/authfailed/domru-helper",
+      },
+    }),
+  ],
+});
