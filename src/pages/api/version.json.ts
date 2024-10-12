34ca0d3ec3b5647098abc35b47be9c585de92be1
Roman Chursanov <authfailed@gmail.com>
2024-10-12 07:09:36 +0500

    Добавлен файл версий расширения в API

--- src/pages/api/version.json.ts
+++ src/pages/api/version.json.ts
@@ -13,15 +13,12 @@ export async function GET() {
                                     },
                                     {
                                         "version": "2.3",
-                                        "update_link": "https://github.com/AuthFailed/domru-helper/releases/download/2.3/domhelper.xpi",
-                                        "update_hash": "sha256:fe93c2156f05f20621df1723b0f39c8ab28cdbeec342efa95535d3abff932096"
+                                        "update_link": "https://github.com/AuthFailed/domru-helper/releases/download/2.3/domhelper.xpi"
                                     },
                                     {
                                         "version": "2.6",
                                         "update_link": "https://github.com/AuthFailed/domru-helper/releases/download/2.6/domhelper.xpi",
-                                        "applications": {
-                                            "gecko": {"strict_min_version": "44"}
-                                        }
+                                        "update_info_url": "https://helper.chrsnv.ru/wiki/versions/2-6"
                                     },
                                     {
                                         "version": "2.8.2",
