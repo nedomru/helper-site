16d7afce1b8cfcbf2995eb30164473d71d5c4f77
Roman Chursanov <authfailed@gmail.com>
2024-11-10 11:53:41 +0500

    Фикс хедеров в API аварийности

--- src/pages/api/octp.json.ts
+++ src/pages/api/octp.json.ts
@@ -59,7 +59,9 @@ export const GET = async () => {
     headers: {
       'Content-Type': 'application/json',
       'Cache-Control': 'no-cache',
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin': '*',
+      "Access-Control-Allow-Methods": "GET",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization"
     }
   });
 };
