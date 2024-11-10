d69ef67342e624483b3d50a7b1ea4b3a7e2bf010
Roman Chursanov <authfailed@gmail.com>
2024-11-10 15:31:54 +0500

    Добавлен API версии расширения для автоматического обновления

---
+++ src/pages/api/versions.json.ts
@@ -0,0 +1,31 @@
+export async function GET() {
+    return new Response(
+        JSON.stringify({
+                "addons": {
+                    "{724d1c91-059e-4392-8478-494513fc8241}": {
+                        "updates": [
+                            {
+                                "version": "2.2",
+                                "update_link": "https://github.com/AuthFailed/domru-helper/releases/download/2.2/domhelper.xpi"
+                            },
+                            {
+                                "version": "2.3",
+                                "update_link": "https://github.com/AuthFailed/domru-helper/releases/download/2.3/domhelper.xpi"
+                            },
+                            {
+                                "version": "2.6",
+                                "update_link": "https://github.com/AuthFailed/domru-helper/releases/download/2.6/domhelper.xpi",
+                                "update_info_url": "https://helper.chrsnv.ru/wiki/versions/2-6/"
+                            },
+                            {
+                                "version": "2.8.2",
+                                "update_link": "https://github.com/AuthFailed/domru-helper/releases/download/2.6/domhelper.xpi",
+                                "update_info_url": "https://helper.chrsnv.ru/wiki/versions/2-8-2/"
+                            }
+                        ]
+                    }
+                }
+            }
+        )
+    )
+}
