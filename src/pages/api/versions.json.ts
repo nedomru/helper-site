b1f731145d2be570ac5e397e7b6a62ab7cb03862
Roman Chursanov <authfailed@gmail.com>
2024-12-13 21:45:39 +0500

    Обновление версии расширения

--- src/pages/api/versions.json.ts
+++ src/pages/api/versions.json.ts
@@ -46,6 +46,11 @@ export async function GET() {
               update_link:
                 "https://github.com/AuthFailed/domru-helper/releases/download/2.9.4/domhelper.xpi",
             },
+            {
+              version: "2.9.4.1",
+              update_link:
+                "https://github.com/AuthFailed/domru-helper/releases/download/2.9.4.1/domhelper.xpi",
+            },
           ],
         },
       },
